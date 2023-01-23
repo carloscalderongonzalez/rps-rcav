9110decd6ea224d4107b2bf9678f5899bbe24610
ccalderong94 <carloscalderon@chicagobooth.edu>
2023-01-23 20:23:14 +0000

    1.0

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   layout(false)
 
-  # Add your actions below this line
-  # ================================
-
+  def play_rock
+    self.redirect_to("https://www.wikipedia.org")
+  end
 end
