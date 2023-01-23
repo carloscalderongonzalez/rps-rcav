874ec188d8e35368c4204d8715c366df6ab4f4b0
ccalderong94 <carloscalderon@chicagobooth.edu>
2023-01-23 20:50:48 +0000

    Rock works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,14 @@ class ApplicationController < ActionController::Base
   layout(false)
 
   def play_rock
+    render({:template => "game_templates/user_rock.html.erb"})
+  end
+
+  def play_scissors
+    self.redirect_to("https://www.wikipedia.org")
+  end
+
+  def play_paper
     self.redirect_to("https://www.wikipedia.org")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
   get("/rock", {:controller => "application", :action => "play_rock"})
+  get("/scissors", {:controller => "application", :action => "play_scissors"})
+  get("/paper", {:controller => "application", :action => "play_paper"})
 
 end
